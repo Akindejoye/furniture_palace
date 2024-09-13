7b4a0f6877ec80d39fa1f7bbdea4c133761c4fb4
Akindejoye <tydruhill@gmail.com>
2024-09-13 20:01:18 +0100

    Cart Tab implementation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import iconCart from "../assets/images/iconCart.png";
+import { toggleStatusTab } from "../stores/cart";
 
 const Header = () => {
   const [totalQuantity, setTotalQuantity] = useState(0);
+
+  const dispatch = useDispatch();
 
   const carts = useSelector((store) => store.cart.items);
 
@@ -12,14 +15,21 @@ const Header = () => {
     let total = 0;
     carts.forEach((item) => (total += item.quantity));
     setTotalQuantity(total);
-  });
+  }, [carts]);
+
+  const handleOpenTabCart = () => {
+    dispatch(toggleStatusTab());
+  };
 
   return (
     <header className="flex justify-between items-center mb-5">
       <Link to="/" className="text-xl font-semibold">
         Home
       </Link>
-      <div className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative">
+      <div
+        className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative"
+        onClick={handleOpenTabCart}
+      >
         <img src={iconCart} alt="Cart" className="w-6" />
         <span className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center">
           {totalQuantity}
