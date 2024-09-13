cfd018d7eb370b463c3c0a6fb7b40b59af3df9c1
Akindejoye <tydruhill@gmail.com>
2024-09-13 18:45:17 +0100

    Product number linked to cart icon

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import iconCart from "../assets/images/iconCart.png";
 
 const Header = () => {
+  const [totalQuantity, setTotalQuantity] = useState(0);
+
+  const carts = useSelector((store) => store.cart.items);
+
+  useEffect(() => {
+    let total = 0;
+    carts.forEach((item) => (total += item.quantity));
+    setTotalQuantity(total);
+  });
+
   return (
     <header className="flex justify-between items-center mb-5">
       <Link to="/" className="text-xl font-semibold">
@@ -10,7 +22,7 @@ const Header = () => {
       <div className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative">
         <img src={iconCart} alt="Cart" className="w-6" />
         <span className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center">
-          0
+          {totalQuantity}
         </span>
       </div>
     </header>
